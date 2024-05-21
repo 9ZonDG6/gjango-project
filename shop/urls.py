fdd8c657b7825febdf4c328cfde452342ae3f91e
ZonD <i@zond96.ru>
2024-05-21 11:16:48 +0500

    Update project

--- shop/urls.py
+++ shop/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import *
+from .views import IndexView, CategoryView, ProductView, Redirect
 
 urlpatterns = [
     path('', IndexView.as_view(), name='home'),
